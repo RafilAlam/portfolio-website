47423fa1a5c6613f07f656f6e09f6c807bc3ab32
Memeathon <rafilalam009@gmail.com>
2025-06-15 22:32:49 +0800

    Project Cards Finished

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,48 +1,65 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { useGSAP } from "@gsap/react";
 import Image from "next/image";
 import Link from "next/link";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollSmoother);
+
 import { Button } from "@/components/ui/button";
 import Navbar from "@/components/ui/navbar";
 import Hero from "@/components/ui/hero";
+import ProjectCard from "@/components/ui/projectcard"
 
 export default function Home() {
+  const smootherRef = useRef<ScrollSmoother | null>(null);
   const arrowref = useRef(null);
+
+  useEffect(() => {
+    const smoother = ScrollSmoother.create({
+      wrapper: "#smooth-scroll",
+      content: "#body",
+      smooth: 1,
+      normalizeScroll: true,
+      effects: false,
+    });
+
+    return () => {
+      // kills on unmount to prevent memory leak
+      smootherRef.current?.kill();
+    }
+  })
 
   useEffect(() => {
     gsap.timeline({repeat: -1, repeatDelay: 0.5}).to(arrowref.current, {marginTop: "20px", opacity: 1, duration: 0.8, ease: "power4.out"}).to(arrowref.current, {marginTop: "0px", opacity: 0.5, duration: 1, ease: "power3.out"})
   }, [])
 
   return (
-    <div id="body">
-      <div id="section-nav" className="z-10 flex fixed w-full pt-[24px] pl-[48px] pr-[48px] bg-linear-[#0006,#0000]">
-        <Navbar />
-      </div>
-      <div id="section-hero" className="flex z-1 pt-[160px] pb-[4] pl-[48px] pr-[48px] justify-center relative bg-radial-[circle_closest-corner_at_50%_0,_#f2f2f226,_#0000]">
-        <div id="hero-fade" className="flex absolute z-5 h-[200px] pt-[48px] inset-0 top-[auto] justify-center bg-linear-[to_bottom,_#10101000,_#101010_60%]">
-          <div id="scroll-wrapper" className="">
-            <Image ref={arrowref} src="https://web.archive.org/web/20240904115935im_/https://cdn.prod.website-files.com/63dcb6e1a80e9454b630f4c4/63e206eedd964a5c5db19c91_23-icon-arrow-down.svg" width="40" height="40" alt="" className="relative block opacity-50"></Image>
+      <div id="smooth-scroll">
+        <div id="body">
+          <div id="section-nav" className="z-10 flex fixed w-full pt-[24px] pl-[48px] pr-[48px] bg-linear-[#0006,#0000]">
+            <Navbar />
+          </div>
+          <div id="section-hero" className="flex z-1 pt-[160px] pb-[4] pl-[48px] pr-[48px] justify-center relative bg-radial-[circle_closest-corner_at_50%_0,_#f2f2f226,_#0000]">
+            <div id="hero-fade" className="flex absolute z-5 h-[200px] pt-[48px] inset-0 top-[auto] justify-center bg-linear-[to_bottom,_#10101000,_#101010_60%]">
+              <div id="scroll-wrapper" className="">
+                <Image ref={arrowref} src="https://web.archive.org/web/20240904115935im_/https://cdn.prod.website-files.com/63dcb6e1a80e9454b630f4c4/63e206eedd964a5c5db19c91_23-icon-arrow-down.svg" width="40" height="40" alt="" className="relative block opacity-50"></Image>
+              </div>
+            </div>
+            <Hero />
+            <div id="hero-noise" className="z-0 top-0 absolute inset-0 w-full h-full opacity-20 bg-size-[200px] bg-blend-overlay bg-[url('https://web.archive.org/web/20240904144252im_/https://cdn.prod.website-files.com/63dcb6e1a80e9454b630f4c4/644af0299bc01254815ae442_noise-overlay-2.png')]"></div>
+          </div>
+          <div id="section-projects" className="z-1 w-full flex flex-col relative gap-[48px] pt-[24px] pb-[200px] pl-[28px] pr-[28px] items-center bg-[#101010]">
+            <ProjectCard title="Art Buddy" glowcolor="#035" company="Pasir Ris Secondary, '25" description="description" src="/project-thumbnails/artbuddy.png" />
+            <ProjectCard title="Art Buddy" glowcolor="#035" company="Pasir Ris Secondary, '25" description="description" src="/project-thumbnails/artbuddy.png" />
+            <ProjectCard title="Art Buddy" glowcolor="#035" company="Pasir Ris Secondary, '25" description="description" src="/project-thumbnails/artbuddy.png" />
           </div>
         </div>
-        <Hero />
-        <div id="hero-noise" className="z-0 top-0 absolute inset-0 w-full h-full opacity-20 bg-size-[200px] bg-blend-overlay bg-[url('https://web.archive.org/web/20240904144252im_/https://cdn.prod.website-files.com/63dcb6e1a80e9454b630f4c4/644af0299bc01254815ae442_noise-overlay-2.png')]"></div>
       </div>
-      <div id="section-projects" className="z-1 flex flex-col relative pt-[24px] pb-[200px] pl-[48px] pr-[48px] bg-[#101010]">
-        
-      </div>
-    </div>
   );
 }
---
+++ portfolio/components/ui/projectcard.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ProjectCard(props: any) {
+    return (
+        <div id="card" className="z-3 relative lg:max-w-[1044px] max-w-[846px] lg:max-h-[696px] max-h-[588px] w-full">
+            <div id="card-glare-outer" className="absolute inset-0 z-1 h-[1px] bg-linear-[to_right,_#0000_5%,_#fffc_35%,_#fff_50%,_#fffc_65%,_#0000_95%]"></div>
+            <div id="card-outline" className="p-[9px] w-full lg:h-[696px] h-[588px] outline-[1px] outline-offset-[-1px] outline-[#f2f2f226] rounded-[24px] bg-[#f2f2f20a] shadow-[0_40px_80px_#00000080]">
+                <div id="card-glare-inner" className="h-[2px] mb-[-1px] bg-linear-[to_right,_#0000_10%,_#fff_50%,_#0000_90%]"></div>
+                <div id="card-main" className="relative rounded-[16px] w-full h-full border-[1px] border-[#3d3d3d] overflow-hidden bg-linear-[190deg,_#252525,_#101010] shadow-[inset_0_0_10px_#0000001a,_0_0_12px_#0006] group hover:border-[#888] transition-colors duration-400 ease-[cubic-bezier(.165,.84,.44,1)]">
+                    <div id="card-content" className="flex relative flex-col gap-[64px] z-5 pt-[32px] pl-[28px] pr-[28px]">
+                        <div id="card-top" className="z-1 flex flex-col relative gap-[8px]">
+                            <div id="title-row" className="flex justify-between text-shadow-[0_-2px_10px_#fff3] tracking-0">
+                                <div id="title" className="flex text-[24px] font-medium">{props.title}</div>
+                                <Image id="arrow" src="https://web.archive.org/web/20240904115935im_/https://cdn.prod.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg" width="32" height="32" alt=""></Image>
+                            </div>
+                            <div id="description-row" className="text-[#f2f2f280]">
+                                <span id="company" className="text-[#f2f2f2cc]">{props.company} </span>
+                                — {props.description}
+                            </div>
+                        </div>
+                        <div id="card-thumbnail" className="flex justify-center">
+                            <img className="z-1 relative rounded-[7px] shadow-[0_40px_50px_10px_#00000040] max-w-9/10 w-full h-full group-hover:-translate-y-5 duration-300 ease-out transition-transform" src={props.src} />
+                        </div>
+                    </div>
+                    <div id="card-color" style={{ backgroundImage: `radial-gradient(circle at 50% 0, #7fcfff33, #0000 80%), radial-gradient(circle at 50% 0, ${props.glowcolor}, #0000)` }} className="absolute group-hover:opacity-100 opacity-0 inset-0 w-full h-full group-hover:ease-inout transition-opacity ease-out group-hover:duration-400 duration-700"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
