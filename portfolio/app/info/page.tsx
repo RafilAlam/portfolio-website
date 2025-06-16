d81e98d11c5b384116b74e771dd60f79081cf7c2
Memeathon <rafilalam009@gmail.com>
2025-06-16 23:13:30 +0800

    Polished Work page and Finished boilerplate for info page

--- portfolio/app/info/page.tsx
+++ portfolio/app/info/page.tsx
@@ -67,61 +67,63 @@ export default function Home() {
   }, [])
 
   return (
-      <div id="body" className="min-h-screen bg-radial-[circle_closest-corner_at_50%_0,_#f2f2f226,_#0000]">
+      <div id="body">
         <div id="section-nav" className="z-10 flex fixed w-full pt-[24px] pl-[48px] pr-[48px] bg-linear-[#0006,#0000]">
               <Navbar tab="2" />
         </div>
         <div id="smooth-scroll">
           <div id="smooth-content">
-            <div id="section-about" className="flex flex-col relative gap-[80px] z-1 pt-[160px] pb-[120px] pl-[48px] pr-[48px] justify-center relative">
-              <div id="fade" className="z-5 flex flex-col absolute inset-0 top-[auto] items-center h-[200px] pt-[48px] bg-linear-[to_bottom,_#10101000,_#101010_60%]">
-              </div>
-              <div id="title-wrapper" className="flex flex-col gap-[24px]">
-                  <Header text="ABOUT ME" />
-                  <div id="text-hero" className="lg:text-[85px] text-[8.2vw] font-medium leading-18 tracking-[-2px] text-shadow-[0_5px_10px_#0057ff26,_0_-5px_10px_#ff5a001a,_0_-5px_25px_#ffffff4d]">I craft tools & platforms that <span id="text-hero-serif" className="font-gloock italic bg-clip-text text-transparent bg-linear-[to_bottom,_#f2f2f2_50%,_transparent_100%] text-shadow-[0_5px_15px_#0057ff1a,_0_-5px_15px_#ff5a0014,_0_0_30px_#fff3]">empower.</span></div>
-              </div>
-              <div id="info-block" className="grid grid-cols-2 gap-[48px]">
-                <div id="col-left" className="flex flex-col relative w-full z-3">
-                  <ImgCard src="" />
-                  <div id="text" className="flex flex-1 px-[24px] tracking-[0.4px] leading-[150%] text-[16px] font-light text-[#f2f2f299]">
-                    <span id="emphasis" className="text-[18px] font-medium tracking-[0.4px] leading-[150%] text-[#f2f2f2e6]"></span>
+            <div id="bg" className="bg-radial-[circle_closest-corner_at_50%_0,_#f2f2f226,_#0000]">
+                <div id="section-about" className="flex flex-col relative gap-[80px] mx-[auto] z-1 pt-[172px] pb-[120px] lg:px-[0px] px-[48px]  lg:max-w-[1044px] justify-center">
+                  <div id="fade" className="z-5 flex flex-col absolute inset-0 top-[auto] items-center h-[200px] pt-[48px] bg-linear-[to_bottom,_#10101000,_#101010_60%]">
                   </div>
-                </div>
-                <div id="col-right" className="flex flex-col relative w-full z-3 gap-[48px] pt-[48px]">
-                  <div id="text">
-                    <span id="emphasis" className="text-[18px] font-medium tracking-[0.4px] leading-[150%] text-[#f2f2f2e6]"></span>
+                  <div id="title-wrapper" className="flex flex-col gap-[24px]">
+                      <Header text="ABOUT ME" />
+                      <div id="text-hero" className="lg:text-[56px] text-[42px] font-medium lg:leading-[56px] leading-[110%] tracking-[-1px] text-shadow-[0_4px_10px_#0057ff33,_0_-4px_10px_#ff5a0026,_0_0_30px_#fff3]">I'm passionate about creating beautiful products that <span id="text-hero-serif" className="font-gloock italic bg-clip-text text-transparent bg-linear-[to_bottom,_#f2f2f2_50%,_#f2f2f233] text-shadow-[0_4px_6px_#0057ff26,_0_-4px_6px_#ff5a001a,_0_0_20px_#ffffff40]">solve real problems.</span></div>
                   </div>
-                  <ImgCard src="" />
-                </div>
-              </div>
-              <div id="hero-noise" className="z-0 top-0 absolute inset-0 w-full h-full opacity-20 bg-size-[200px] bg-blend-overlay bg-[url('https://web.archive.org/web/20240904144252im_/https://cdn.prod.website-files.com/63dcb6e1a80e9454b630f4c4/644af0299bc01254815ae442_noise-overlay-2.png')]"></div>
-            </div>
-
-            <div id="section-divider" className="flex flex-col items-center">
-              <div id="container-general" className="flex flex-col w-[882px] gap-[96px]">
-                <div id="divider-wrapper" className="flex gap-[8px] items-center">
-                  <div id="divider-line" className="flex-1 h-[1px] bg-[#f2f2f233]"></div>
-                </div>
-              </div>
-            </div>
-
-            <div id="section-experience" className="flex flex-col pt-[72px] pb-[120px] relative z-1 gap-[56px] items-center">
-              <div id="content" className="flex flex-col gap-[64px] align-start w-[882px]">
-                <Header text="EXPERIENCE" />
-                <div id="block" className="grid gap-x-[48px] gap-y-[16px] justify-between grid-cols-2">
-                  <div id="content-leading" className="flex flex-col gap-[16px] font-medium -tracking-[0.1px] text-[40px] leading-[125%]">Pasir Ris Secondary</div>
-                  <div id="content-experience" className="flex flex-col gap-[20px] pt-0">
-                    <div id="title-date" className="flex flex-col gap-[8px]">
-                      <div id="title" className="tracking-[0.1px] shrink-[auto] pr-[24px] text-[24px] font-medium leading-[125%]">Chairperson of Tinkers' Club</div>
-                      <div id="date" className="text-[#f2f2f266] text-[16px] font-normal leading-[125%]">05/'24 - 06/'25</div>
+                  <div id="info-block" className="grid grid-cols-2 gap-[48px]">
+                    <div id="col-left" className="flex flex-col relative w-full z-3">
+                      <ImgCard />
+                      <div id="text" className="flex flex-1 px-[24px] tracking-[0.4px] leading-[150%] text-[16px] font-light text-[#f2f2f299]">
+                        <span id="emphasis" className="text-[18px] font-medium tracking-[0.4px] leading-[150%] text-[#f2f2f2e6]"></span>
+                      </div>
                     </div>
-                    <div id="body-info" className="flex-1 leading-[150%] font-light text-[16px] text-[#f2f2f299]">I ran Tinkers' Club and managed activities for members.</div>
+                    <div id="col-right" className="flex flex-col relative w-full z-3 gap-[48px] pt-[48px]">
+                      <div id="text">
+                        <span id="emphasis" className="text-[18px] font-medium tracking-[0.4px] leading-[150%] text-[#f2f2f2e6]"></span>
+                      </div>
+                      <ImgCard />
+                    </div>
+                  </div>
+                  <div id="hero-noise" className="z-0 absolute inset-0 w-full h-full opacity-20 bg-size-[200px] bg-blend-overlay bg-[url('https://web.archive.org/web/20240904144252im_/https://cdn.prod.website-files.com/63dcb6e1a80e9454b630f4c4/644af0299bc01254815ae442_noise-overlay-2.png')]"></div>
+                </div>
+              
+                <div id="section-divider" className="flex flex-col items-center">
+                  <div id="container-general" className="flex flex-col w-[882px] gap-[96px]">
+                    <div id="divider-wrapper" className="flex gap-[8px] items-center">
+                      <div id="divider-line" className="flex-1 h-[1px] bg-[#f2f2f233]"></div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div id="section-footer" className="flex relative justify-center border-t-[1px] border-t-[#f2f2f21a] pt-[88px] pb-[80px] px-[48px]">
-              <Footer />
+              
+                <div id="section-experience" className="flex flex-col pt-[72px] pb-[120px] relative z-1 gap-[56px] items-center">
+                  <div id="content" className="flex flex-col gap-[64px] align-start w-[882px]">
+                    <Header text="EXPERIENCE" />
+                    <div id="block" className="grid gap-x-[48px] gap-y-[16px] justify-between grid-cols-2">
+                      <div id="content-leading" className="flex flex-col gap-[16px] font-medium -tracking-[0.1px] text-[40px] leading-[125%]">Pasir Ris Secondary</div>
+                      <div id="content-experience" className="flex flex-col gap-[20px] pt-[20px]">
+                        <div id="title-date" className="flex flex-col gap-[8px]">
+                          <div id="title" className="tracking-[0.1px] shrink-[auto] pr-[24px] text-[24px] font-medium leading-[125%]">Chairperson of Tinkers' Club</div>
+                          <div id="date" className="text-[#f2f2f266] text-[16px] font-normal leading-[125%]">05/'24 - 06/'25</div>
+                        </div>
+                        <div id="body-info" className="flex-1 leading-[150%] font-normal tracking-[0.4px] text-[16px] text-[#f2f2f299]">I ran Tinkers' Club and managed activities for members.</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div id="section-footer" className="flex relative justify-center border-t-[1px] border-t-[#f2f2f21a] pt-[88px] pb-[80px] px-[48px]">
+                  <Footer />
+                </div>
             </div>
           </div>
         </div>
--- portfolio/components/ui/hero.tsx
+++ portfolio/components/ui/hero.tsx
@@ -9,8 +9,15 @@ export default function Hero() {
             <div id="window-glare-outer" className="absolute inset-0 z-1 h-[1px] bg-linear-[to_right,_#0000_5%,_#fffc_35%,_#fff_50%,_#fffc_65%,_#0000_95%]"></div>
             <div id="window-outline" className="p-[9px] lg:max-w-[1044px] max-w-[882px] w-full outline-[1px] outline-offset-[-1px] outline-[#f2f2f226] rounded-[24px] bg-radial-[circle_farthest-side_at_50%_0,_#f2f2f233,_#0000] shadow-[inset_0_0_8px_#0006,_0_0_60px_#0003,_0_30px_120px_#000c]">
                 <div id="window-glare-inner" className="h-[2px] mb-[-2px] bg-linear-[to_right,_#0000_10%,_#fff_50%,_#0000_90%]"></div>
-                <div id="window-main" className="relative rounded-[16px] w-full backdrop-blur-[100px] border-[1px] border-[#f2f2f24d] overflow-hidden shadow-[inset_0_0_10px_#0000001a,_0_0_12px_#0006]">
-                    <div id="window-bar" className="flex z-5 w-full h-[40px] backdrop-blur-[40px] bg-linear-[to_right,_#f2f2f21a,_#f2f2f280_50%,_#f2f2f21a] shadow-[0_10px_20px_4px_#0003]"></div>
+                <div id="window-main" className="relative rounded-[16px] w-full border-[1px] border-[#f2f2f24d] overflow-hidden shadow-[inset_0_0_10px_#0000001a,_0_0_12px_#0006]">
+                    <div id="window-bar" className="flex z-5 items-center pl-[20px] pr-[12px] justify-between w-full h-[40px] backdrop-blur-[40px] bg-linear-[to_right,_#f2f2f21a,_#f2f2f280_50%,_#f2f2f21a] shadow-[0_10px_20px_4px_#0003]">
+                        <div id="dots-wrapper" className="flex gap-[10px]">
+                            <div id="dot1" className="w-[12px] h-[12px] border-[#10101040] rounded-[100px] bg-[#ff878b] shadow-[0_0_20px_2px_#ff878b]"></div>
+                            <div id="dot2" className="w-[12px] h-[12px] border-[#10101040] rounded-[100px] bg-[#ffc787] shadow-[0_0_20px_2px_#ffc787]"></div>
+                            <div id="dot3" className="w-[12px] h-[12px] border-[#10101040] rounded-[100px] bg-[#8bff87] shadow-[0_0_20px_2px_#8bff87]"></div>
+                        </div>
+                        <Image className="opacity-20" width="24" height="24" alt="" src="https://web.archive.org/web/20250114204118im_/https://cdn.prod.website-files.com/63dcb6e1a80e9454b630f4c4/64647bd0f92c6bb858b22871_icon-plus.svg"></Image>
+                    </div>
                     <div id="window-content" className="flex flex-col gap-[64px] z-5 lg:pb-[200px] lg:pt-[96px] pt-[8vw] pl-[5vw] pr-[5vw] pb-[26vw] bg-[#101010b3]">
                         <div id="text-hero" className="lg:text-[85px] text-[8.2vw] font-medium leading-18 tracking-[-2px] text-shadow-[0_5px_10px_#0057ff26,_0_-5px_10px_#ff5a001a,_0_-5px_25px_#ffffff4d]">I craft tools & platforms that <span id="text-hero-serif" className="font-gloock italic bg-clip-text text-transparent bg-linear-[to_bottom,_#f2f2f2_50%,_transparent_100%] text-shadow-[0_5px_15px_#0057ff1a,_0_-5px_15px_#ff5a0014,_0_0_30px_#fff3]">empower.</span></div>
                         <div id="about-hero" className="grid grid-cols-2 gap-[2vw]">
