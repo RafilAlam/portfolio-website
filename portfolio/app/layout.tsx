096053e7c6206ca66932e5d5de78632b1c77035d
Memeathon <rafilalam009@gmail.com>
2025-06-20 22:27:26 +0800

    Update layout.tsx

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -3,7 +3,7 @@ import { Yellowtail } from "next/font/google";
 import "./globals.css";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const _yellowtail = Yellowtail({
+const yellowtail = Yellowtail({
   subsets: ["latin"],
   weight: ["400"],
   variable: "--font-yellowtail"
