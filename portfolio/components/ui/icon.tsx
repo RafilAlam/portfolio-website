e0dad40757952e1e883b0b5c5ab79bcd9e029063
Memeathon <rafilalam009@gmail.com>
2025-06-27 19:12:49 +0800

    Update icon.tsx

--- portfolio/components/ui/icon.tsx
+++ portfolio/components/ui/icon.tsx
@@ -9,7 +9,7 @@ type props = {
 
 export default function Icon(props: props) {
     return (
-        <div id="icon" className={`flex-grow ${props.hidden ? "flex" : "hidden"} justify-center  max-w-[60.95px] bg-[#181818] w-[42px] sm:w-[5vw] p-2 aspect-square rounded-[8px]`} href={props.href} target="_blank">
+        <div id="icon" className={`flex-grow ${props.hidden ? "flex" : "hidden"} justify-center  max-w-[60.95px] bg-[#181818] w-[42px] sm:w-[5vw] p-2 aspect-square rounded-[8px]`}>
             <img className="z-1 rounded-[4px]  aspect-square" src={props.src} alt={props.name} />
         </div>
     )
